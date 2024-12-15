3414b77c55f0083557fa0b2af0fc168027edc637
Daniel Bond <dbond@kse.org.ua>
2024-12-15 11:51:54 +0200

    Initial Commit

---
+++ BPlayer/BPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPlayerApp.swift
+//  BPlayer
+//
+//  Created by Daniel Bond on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
